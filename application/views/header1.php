4c224854ecc373724ff13e6b86fa5bd21009ab5b
Arman <grigoryan.arman@bk.ru>
2019-04-10 02:35:58 +0400

    Project init

---
+++ application/views/header1.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>NUACALibrary</title>
+    <link rel="icon"  href="./favicon.png">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/header1.css">
+    <script src="./assets/js/jquery-3.3.1.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">
+        <img src="./assets/images/logo.png" alt="">NUACA Library</a>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Գլխավոր <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Մուտք գործել</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Հետադարձ կապ</a>
+            </li>
+
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Որոնում" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Որոնում</button>
+        </form>
+    </div>
+</nav>
---
+++ application/views/home.php
@@ -0,0 +1,146 @@
+<div class="row mrg_top">
+    <div class="col-md-4">
+        <div class="accordion" id="accordionExample">
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link btn-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Ճարտարապետության ֆակուլտետ
+                        </button>
+                    </h5>
+                </div>
+
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <ul>
+                            <li>Ճարտարապետական նախագծման եվ ճարտարապետական միջավայրի դիզայնի ամբիոն</li>
+                            <li>Ճարտարապետության տեսության, պատմաճարտարապետական ժառանգության վերականգման, վերակառուցման, գեղեցիկ արվեստի եւ պատմության ամբիոն</li>
+                            <li>Քաղաքաշինության ամբիոն</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingTwo">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link collapsed btn-left" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Շինարարության ֆակուլտետ
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <ul>
+                            <li>Ճանապարհների և կամուրջների ամբիոն</li>
+                            <li>Շինարարական նյութերի, պատրաստվածքների եվ կոնստրուկցիաների արտադրության տեխնոլոգիայի ամբիոն</li>
+                            <li>Շինարարական մեխանիկայի ամբիոն</li>
+                            <li>Շինարարական կոնստրուկցիաների ամբիոն</li>
+                            <li>Ինժեներական գեոդեզիայի ամբիոն</li>
+                            <li>Հիդրոշինարարության, ջրային համակարգերի և  հիդրոէլեկտրակայանների ամբիոն</li>
+                            <li>Շինարարական արտադրության տեխնոլոգիայի և կազմակերպման ամբիոն</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingThree">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link collapsed btn-left" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Քաղաքային տնտեսության եվ էկոլոգիայի ֆակուլտետ
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <ul>
+                            <li>Ջերմագազամատակարարման և օդափոխության ամբիոն</li>
+                            <li>Տեխնիկական թարգմանության ամբիոն</li>
+                            <li>Քիմիայի, կապակցող նյութերի և սիլիկատների ամբիոն</li>
+                            <li>Գեոէկոլոգիայի և կենսաանվտանգության ամբիոն</li>
+                            <li>Հիդրավլիկայի ամբիոն</li>
+                            <li>Շինարարական մեքենաների և երթեվեկության կազմակերպման ամբիոն</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingFour">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link collapsed btn-left" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            Կառավարման եվ տեխնոլոգիայի ֆակուլտետ
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <ul>
+                            <li>Էկոնոմիկայի, իրավունքի և կառավարման ամբիոն</li>
+                            <li>Ինֆորմատիկայի, հաշվողական տեխնիկայի և կառավարման համակարգերի ամբիոն</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingFive">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link collapsed btn-left" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                            Դիզայնի ֆակուլտետ
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <ul>
+                            <li>Գծանկարի, գունանկարի և քանդակի ամբիոն</li>
+                            <li>Դիզայնի ամբիոն</li>
+                            <li>Ինտերիերի և էքստերիերի դիզայնի  ամբիոն</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-8"><!--
+        <div class="row">
+            <div class="img"><img src="assets/images/book-example.jpg" alt="" width="100"></div>
+            <div class="name">Name</div>
+            <div class="author">Author</div>
+            <div class="chairs">Chairs</div>
+            <div class="download">Download</div>
+        </div>-->
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">Նկար</th>
+                <th scope="col">Անուն</th>
+                <th scope="col">Հեղինակ</th>
+                <th scope="col">Ամբիոն</th>
+                <th scope="col">Ներբեռնել</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row"><img src="assets/images/book-example.jpg" alt="" width="100"></th>
+                <td>Անուն</td>
+                <td>Հեղինակ</td>
+                <td>Ամբիոն</td>
+                <td>Ներբեռնել</td>
+            </tr>
+            <tr>
+                <th scope="row"><img src="assets/images/book-example.jpg" alt="" width="100"></th>
+                <td>Անուն</td>
+                <td>Հեղինակ</td>
+                <td>Ամբիոն</td>
+                <td>Ներբեռնել</td>
+            </tr>
+            <tr>
+                <th scope="row"><img src="assets/images/book-example.jpg" alt="" width="100"></th>
+                <td>Անուն</td>
+                <td>Հեղինակ</td>
+                <td>Ամբիոն</td>
+                <td>Ներբեռնել</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
